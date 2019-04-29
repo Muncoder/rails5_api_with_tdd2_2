0cd83b359f4a4d500afe30efdb04b6c3eda948e4
Munir Uddin <munirchittagong@yahoo.com>
2019-04-29 20:35:16 +0100

    34. authentication service success

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,12 +1,32 @@
 class UserAuthenticator
   class AuthenticationError < StandardError; end
+
   attr_reader :user
 
   def initialize(code)
-
+    @code = code
   end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+    token = client.exchange_code_for_token(code)
+    # require 'pry'; binding.pry
+
+    if token.try(:error).present?
+      raise AuthenticationError
+    else
+      user_client = Octokit::Client.new(
+        access_token: token)
+      user_data = user_client.user.to_h.
+        slice(:login, :url, :avatar_url, :name)
+      User.create(user_data.merge(provider: 'github'))
+    end
   end
+
+  private
+
+  attr_reader :code
 end
